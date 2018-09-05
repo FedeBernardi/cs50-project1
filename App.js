04b2f9718c9f083d2099b3ab53952165072ad4a1
FedeBernardi <fedeew94@hotmail.com>
2018-09-05 18:17:47 -0300

    Moving Interval to App Component

--- App.js
+++ App.js
@@ -1,23 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Vibration } from 'react-native';
 import Timer from './components/Timer';
+
+const WORKING_TEXT = "Time to work buddy!",
+      RESTING_TEXT = "You can relax now :)";
 
 export default class App extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            workTimer: 25,
+            workTimer: 0,
             breakTimer: 5,
-            working: true
+            currentTime: {minutes: 0, seconds: 10},
+            isTicking: false,
+            isWork: true
         }
     }
 
+    timerToggle() {
+        let {isTicking} = this.state;
+        this.setState({isTicking: !isTicking});
+
+        if (!isTicking) {
+            this.kickInterval();
+        } else {
+            this.destroyInterval();
+        }
+    }
+
+    kickInterval() {
+        this.interval = setInterval(() => {
+            let {minutes, seconds} = this.state.currentTime;
+            seconds--;
+
+            if (seconds === -1 && minutes) {
+                seconds = 59;
+                minutes--;
+            }
+
+            if (!minutes && !seconds) {
+                Vibration.vibrate();
+                this.switchTimers();
+            } else {
+                this.setState({currentTime: {minutes, seconds}})
+            }
+        }, 1000);
+    }
+
+    destroyInterval() {
+        clearInterval(this.interval);
+    }
+
+    switchTimers() {
+        let {workTimer, breakTimer, isWork} = this.state;
+
+        this.setState({
+            isTicking: false,
+            isWork: !isWork,
+            currentTime: {minutes: !isWork ? workTimer : breakTimer, seconds: 0}
+        });
+        this.destroyInterval();
+    }
+
     render() {
+        let {minutes, seconds} = this.state.currentTime,
+            {isTicking, isWork} = this.state;
+
         return (
             <View style={styles.container}>
+                {isTicking && <Text>{isWork ? WORKING_TEXT : RESTING_TEXT}</Text>}
+                {!isTicking && <Text>Press the button to start the clock!</Text>}
                 <View>
-                    <Timer startingTime={this.state.working ? this.state.workTimer : this.state.breakTimer}></Timer>
+                    <Timer minutes={minutes} seconds={seconds}></Timer>
+                </View>
+                <View>
+                    <Button
+                        title={isTicking ? 'Stop' : 'Start'}
+                        onPress={() => this.timerToggle()}>
+                    </Button>
                 </View>
                 <View>
                     {/*<Button
